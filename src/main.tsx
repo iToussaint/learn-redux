67b450def2ff06f2ee8d9e192c77d0d4bc564632
iToussaint <iradukundatoussaint01@gmail.com>
2026-07-28 00:59:06 +0200

    fix: remove unused App component from main render

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import Router from "./app/router.tsx";
 import { RouterProvider } from "react-router";
 import { Provider } from "react-redux";
@@ -10,9 +9,7 @@ import store from "./app/store.ts";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={Router}>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={Router}></RouterProvider>
     </Provider>
   </StrictMode>,
 );
