0954c85e7009613e37408d71ae8a2b94b6f97977
iToussaint <iradukundatoussaint01@gmail.com>
2026-07-27 23:13:09 +0200

    fix: correct increment and decrement logic in counter slice feat: add Tailwind CSS plugin to Vite configuration

--- src/features/counter/counter.slice.ts
+++ src/features/counter/counter.slice.ts
@@ -9,10 +9,10 @@ const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-      state.count + 1;
+      state.count += 1;
     },
     decrement: (state) => {
-      state.count - 1;
+      state.count -= 1;
     },
   },
 });
